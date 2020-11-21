1f6439dd44ad6859dad58a0af5ad406f2053c4ae
alert9 <tznin@qq.com>
2020-11-21 09:21:37 +0800

    增加fastcopy
    修改新版。增加fastcopy ，

---
+++ src/backsyn/Main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/alert0/backsyn/logger"
+)
+
+func Test_compress7zip(t *testing.T) {
+
+	err := compress7zip("D:/3/DLLLIB", "d:/aaa.7z")
+
+	if err != nil {
+
+		logger.Info("压缩错误")
+	}
+
+	//if !IsPalindrome("detartrated") {
+	//	t.Error(`IsPalindrome("detartrated") = false`)
+	//}
+	//if !IsPalindrome("kayak") {
+	//	t.Error(`IsPalindrome("kayak") = false`)
+	//}
+}
